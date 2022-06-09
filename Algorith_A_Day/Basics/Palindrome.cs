d969810e3ae1af5f6df11c087083de01fd8deb14
nirzaf <mfmfazrin1986@gmail.com>
2022-06-09 22:11:15 +0530

    aaaaa

--- Algorith_A_Day/Basics/Palindrome.cs
+++ Algorith_A_Day/Basics/Palindrome.cs
@@ -4,20 +4,12 @@ namespace Algorithm_A_Day.Basics
 {
     class Palindrome
     {
-        /// <summary>
-        /// Determines if a single word is a palindrome.
-        /// </summary>
-        /// <param name="word">a word</param>
-        /// <returns>true if palindrome, otherwise false</returns>
         static bool IsWordPalindrome(string word)
         {
             if (word == null)
                 throw new ArgumentNullException(nameof(word));
 
-            if (word.Length < 2)
-                return true;
-
-            return IsPalidrome(0, word.Length - 1);
+            return word.Length < 2 || IsPalidrome(0, word.Length - 1);
 
             bool IsPalidrome(int lo, int hi)
             {
