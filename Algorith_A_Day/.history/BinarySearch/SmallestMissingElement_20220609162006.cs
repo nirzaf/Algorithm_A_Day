e936ff442c761a6bead8c00e7f0c52f4807d606f
nirzaf <mfmfazrin1986@gmail.com>
2022-06-09 16:20:37 +0530

    We can deduct that SME will be the index of first el which is not equal to its value. so arr[i] != i

--- Algorith_A_Day/.history/BinarySearch/SmallestMissingElement_20220609162006.cs
+++ Algorith_A_Day/.history/BinarySearch/SmallestMissingElement_20220609162006.cs
@@ -48,3 +48,27 @@
         }
     }
 }
+
+//Recursion section:
+//stack is being used so calls are from botton to the top
+
+//{ 0, 1, 2, 3, 4, 5, 6 }
+
+//SmallestMissingEL(int[] arr, 7, 6) mid ==6 ,arr[mid] != mid, 7 > 6 base call is true 7 is returned
+//SmallestMissingEL(int[] arr, 6, 6) mid ==6 ,arr[mid] == mid
+//SmallestMissingEL(int[] arr, 5, 6) mid ==5 ,arr[mid] == mid
+//SmallestMissingEL(int[] arr, 4, 6) mid ==5 ,arr[mid] == mid
+//SmallestMissingEL(int[] arr, 0, 6) first call, mid ==3 ,arr[mid] == mid 
+
+//{ 1, 2, 3, 4, 5, 6 }
+
+//SmallestMissingEL(int[] arr, 0, -1) mid ==0 ,arr[mid] == mid, 0> -1 base call is true 0 is returned
+//SmallestMissingEL(int[] arr, 0, 1) mid ==0 ,arr[mid] != mid
+//SmallestMissingEL(int[] arr, 0, 5) first call, mid ==2 ,arr[mid] != mid 
+
+//{ 0, 1, 2, 6, 9, 11, 15 }
+//SmallestMissingEL(int[] arr, 3, 2) 3> 2 base call is true 3 is returned
+//SmallestMissingEL(int[] arr, 2, 2) mid ==2 ,arr[mid] == mid
+//SmallestMissingEL(int[] arr, 0, 2) mid ==1 ,arr[mid] == mid arr[1] == 1, !!!
+//SmallestMissingEL(int[] arr, 0, 6) first call, mid ==3 ,arr[mid] != mid 
+
