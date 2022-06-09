128c933d77120772d61f298702b9d81f3d678fa0
nirzaf <mfmfazrin1986@gmail.com>
2022-06-09 16:27:33 +0530

    CollatzIterativly

--- Algorith_A_Day/Basics/CollatzConjecture.cs
+++ Algorith_A_Day/Basics/CollatzConjecture.cs
@@ -14,30 +14,27 @@
                 return 1 + Collatz(3 * n  + 1);
             }
         }
-
-        //c(5) 
-        // 1 + c(15 + 1) c16
-        // 1 + c(16 /2) c8
-        // 1 + c(8 / 2) c4 
-        // 1 + c(4 /2) c2
-        // 1 + c(2 / 2) c1
-        // c(1) = 0 base case
-
+        
         public static int CollatzIterativly(int n)
         {
-            if (n == 1) { return 0; }
+            switch (n)
+            {
+                case 1:
+                    return 0;
+            }
             int counter = 0;
             while(n > 1)
             {
-                if(n % 2 == 0)
+                switch (n % 2)
                 {
-                    counter++;
-                    n = n / 2;
-                }
-                else
-                {
-                    counter++;
-                    n = n * 3 + 1;
+                    case 0:
+                        counter++;
+                        n /= 2;
+                        break;
+                    default:
+                        counter++;
+                        n = n * 3 + 1;
+                        break;
                 }
             }
             return counter;
